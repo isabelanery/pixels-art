b1deb1f8f1846542405745a2d6c97d8161f07ba3
isabelanery <isabelanery242@gmail.com>
2021-12-09 17:14:48 -0300

    Adiciona Pixel Board em JS.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('oi')
+const insidePixelBoard = document.querySelector('#pixel-board');
+
+for (let i = 0; i<5; i+=1) {
+    const createDiv = document.createElement('div'); 
+    createDiv.className = "linePixel"; 
+
+    insidePixelBoard.appendChild(createDiv);
+
+    for (let n = 0; n<5; n+=1) {
+        const createPixel = document.createElement('div'); 
+        createPixel.className = "pixel"; 
+
+        createDiv.appendChild(createPixel);
+    }
+}
