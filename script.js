ee008fa1e1b4a1a7fb149fbdd24fc5873823d4f8
isabelanery <isabelanery242@gmail.com>
2021-12-09 22:48:54 -0300

    Adiciona alterações.

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ for (let i = 0; i<5; i+=1) {
         createDiv.appendChild(createPixel);
     }
 }
+
+// montar uma função que seleciona a cor clicada - adiciona a classe SELECTED e a aplica ao pixel clicado posteriormente, removendo a classe selected
+// criar outra função .onload que seleciona a cor preta 
+// criar uma função que aplica a cor selecionada ao pixel 
+
+// let blackSelect = document.querySelector('.selected'); 
+
+// blackSelect.addEventListener("onload", selectColor);
+
+
+// window.onload = function selectBlack () { 
+//     let black = document.querySelector('#one');
+//     black.className = 'color selected'; 
+
+
+// }
+
+
+
+// function selectColor() {
+//     // adiciona selected a classe da cor selecionada 
+//     // remove selected da classe da cor que estava selecionada anteriormente 
+
+//     target
+// }
+
+// function changeColor() { 
+//     // aplica a cor selecionada ao pixel clicado 
+// }
+
+// blackSelect.addEventListener("onload", selectColor);
+
