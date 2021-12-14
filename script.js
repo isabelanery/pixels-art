cef17d6a5cd2e8116792be636522bd86138f9ba9
isabelanery <isabelanery242@gmail.com>
2021-12-14 20:35:25 -0300

    Adiciona alinhamento.

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 const insidePixelBoard = document.querySelector('#pixel-board');
 
 for (let i = 0; i < 5; i += 1) {
-    const createLi = document.createElement('li'); 
-    createLi.className = "linePixel"; 
+    const createDiv = document.createElement('div'); 
+    createDiv.className = "linePixel"; 
 
-    insidePixelBoard.appendChild(createLi);
+    insidePixelBoard.appendChild(createDiv);
 
     for (let n = 0; n < 5; n += 1) {
         const createPixel = document.createElement('div'); 
         createPixel.className = "pixel"; 
 
-        createLi.appendChild(createPixel);
+        createDiv.appendChild(createPixel);
     }
 }
 
@@ -23,6 +23,8 @@ const blue3 = document.querySelector('#three');
 const colorPalette = document.querySelector('#color-palette');
 const colorPalleteList = document.querySelectorAll('#color-palette');
 let clearbtn = document.querySelector('#clear-board'); 
+
+// armazenam futuros valores; 
 let clickedColor = undefined;
 let clickedPixel = undefined; 
 let color = undefined;
@@ -46,11 +48,13 @@ function removeSelected () {
         blue2.classList.remove('selected'); 
     } else if (blue3.classList.contains('selected')) {
         blue3.classList.remove('selected'); 
+    } else if (colorPalette.classList.contains('selected')) {
+        colorPalette.classList.remove('selected'); 
     }
 }
 
 // seleciona a cor 
-function selectColor () { 
+function classSelected () { 
     if (clickedColor.classList.contains('selected')) {
         color = clickedColor.id; 
     } else { 
@@ -60,36 +64,56 @@ function selectColor () {
     }
 }
 
-// pinta 
-function paint () {
-    clickedPixel.id = color; 
-}
+// // pinta 
+// function paint () {
+//     clickedPixel.id = color; 
+// }
 
-function unpaint () {
-    clickedPixel.remove.id; 
-}
+// function unpaint () {
+//     clickedPixel.remove.id; 
+// }
 
-
-
-
-// verifica qual a cor clicada 
-// armazena a cor em clicked color 
-colorPalette.addEventListener('click', event => { // encontrei esse método no canal do Roger Melo, experimentei e resolvia o que eu queria, dai fui pesquisar mais sobre arrow functions e bubbling effect pra entender melhor o que está acontecendo aqui :) 
+function selectColor (event) {
     clickedColor = event.target;  // o valor é indefinido pois aguarda o click 
     color = clickedColor.id; 
-    selectColor();
-    // console.log(color);
-} )
+    classSelected();
+}
 
+
+black.addEventListener('click', selectColor); 
+blue1.addEventListener('click', selectColor); 
+blue2.addEventListener('click', selectColor); 
+blue3.addEventListener('click', selectColor); 
+
+
+
+// // verifica qual a cor clicada 
+// // armazena a cor em clicked color 
+// colorPalette.addEventListener('click', event => { // encontrei esse método no canal do Roger Melo, experimentei e resolvia o que eu queria, dai fui pesquisar mais sobre arrow functions e bubbling effect pra entender melhor o que está acontecendo aqui :) 
+//     clickedColor = event.target;  // o valor é indefinido pois aguarda o click 
+//     color = clickedColor.id; 
+//     selectColor();
+//     // console.log(color);
+// } )
+
+// colorPalette.addEventListener('click', function (e) { 
+//     clickedColor = e.target;  // o valor é indefinido pois aguarda o click 
+//     color = clickedColor.id; 
+// }) 
+ 
 // verifica qual o pixel clicado 
 insidePixelBoard.addEventListener('click', event => { 
     clickedPixel = event.target; // aguarda o click
-    paint (); 
+
+    // if else paint (); 
     // >>> falta remover a cor 
     // console.log(clickedPixel);
 })
 
-// funçao que reseta o pixel board 
+
+
+
+// // funçao que reseta o pixel board 
 // function clearBoard () { 
 //     for(let index = 0; index < insidePixelBoard.length; index += 1) {
 //         let px = insidePixelBoard[index]; 
